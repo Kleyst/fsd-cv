7faa517790a79071e5c6fbb85cb8dfaeaa06c002
Victor Bukhaltsev <victor_bukhaltsev@mail.ru>
2018-09-14 18:22:18 +0700

    Добавил webpack, scss вместо stylus

---
+++ src/index.js
@@ -0,0 +1,1 @@
+import styles from './scss/style.scss';
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path = require('path');
+
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const pathsToClean = ['dist', 'build'];
+const cleanWebpack = new CleanWebpackPlugin(pathsToClean);
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const extractPug = new HtmlWebpackPlugin({
+  filename: 'index.html',
+  template: 'src/index.pug',
+  inject: false
+})
+
+const pug = {
+  test: /\.pug$/,
+  use: ['html-loader?attrs=false', 'pug-html-loader']
+};
+
+const fonts = {
+  test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+  use: [{
+      loader: 'file-loader',
+      options: {
+        name: '[name].[ext]',
+        outputPath: 'fonts/'
+      }
+  }]
+};
+
+const svg =  {
+  test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+  use: [{
+    loader: 'file-loader',
+    options: {
+      name: '[name].[ext]',
+      outputPath: 'img/'
+    }
+}]
+};
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const extractSCSS =  new MiniCssExtractPlugin({
+  filename: "css/style.css",
+});
+
+const scss = {
+  test: /\.scss$/,
+  use: [
+    MiniCssExtractPlugin.loader,
+    "css-loader",
+    "sass-loader"
+  ]
+};
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+    publicPath: 'dist/'
+  },
+  devServer: {
+    overlay: true
+  },
+  module: {
+    rules: [pug, scss, fonts, svg],
+  },
+  plugins: [
+    extractPug,
+    cleanWebpack,
+    extractSCSS
+  ]
+};
+
+module.exports = config;
